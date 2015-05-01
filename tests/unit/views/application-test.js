6c1aa1fd6393b2d352867d88fc0f82e806ab6121
Mike Andrzejewski <mike.aski@gmail.com>
2015-05-01 18:09:50 +0200

    Fix tests (track project renaming)

--- tests/unit/views/application-test.js
+++ tests/unit/views/application-test.js
@@ -2,8 +2,12 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('view:application');
 
-// Replace this with your real tests.
 test('it exists', function(assert) {
   var view = this.subject();
   assert.ok(view);
+
+  assert.deepEqual(view.get('classNames'), [
+    'ember-view',
+    'c2l-main'
+  ], 'View has expected classes');
 });
